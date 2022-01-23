36ce5c164a405328cfd34064c20d5346096dd3d0
singvis <singvis.dev.outlook.com>
2022-01-22 19:51:13 -0500

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {CardList} from './component/card-list/card-list.component';
+import { Search } from './component/search/search.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+class  App extends Component {
+  constructor(){
+      super();
+      this.state = {
+        searchStr: '',
+        monsters: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({monsters :  users}))  
+  }
+  render() {
+    const {searchStr, monsters} = this.state;    
+    const filteredMonsters = monsters.filter(
+      monster => monster.name.toLowerCase().includes(searchStr.toLowerCase()));
+
+    return (
+      <div className="App">
+      <h1>Monsters Rolodex</h1>
+      <Search placeholder='search monsters' handleChange={e=> this.setState({searchStr: e.target.value})} />                  
+      <CardList monsters= {filteredMonsters}> </CardList>                    
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/component/search/search.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './search.styles.css';
+
+export const Search = ({placeholder, handleChange}) =>(
+    <div>
+    <input type='search' className='search' 
+    placeholder={placeholder} 
+    onChange={handleChange}/>
+    </div>
+);
